e21243f8a72d78467c90ba70a13585e977e9bbe5
WaalerThomas <superxgenesis@gmail.com>
2024-07-25 21:49:01 +0200

    Removed over complicated abstraction in core

--- BudgetBuddy_Core/src/Application.cs
+++ BudgetBuddy_Core/src/Application.cs
@@ -1,26 +1,5 @@
-using BudgetBuddyCore.Interfaces;
-
 namespace BudgetBuddyCore;
 
 public class Application
 {
-    public IAccountRespository AccountRepository { get; }
-    public IGroupRepository GroupRepository { get; }
-    public ICategoryRepository CategoryRepository { get; }
-    public ITransactionRepository TransactionRepository { get; }
-
-    //decimal? availableToBudget;
-
-    public Application(
-        IAccountRespository accountRespository,
-        IGroupRepository groupRepository,
-        ICategoryRepository categoryRepository,
-        ITransactionRepository transactionRepository
-    )
-    {
-        AccountRepository = accountRespository;
-        GroupRepository = groupRepository;
-        CategoryRepository = categoryRepository;
-        TransactionRepository = transactionRepository;
-    }
 }
