e478c47a95f5b0ffa7d27cc245c849fc8bc3a248
TomWaa <superxgenesis@gmail.com>
2024-07-31 13:19:01 +0200

    [test] Created empty transaction test methods

---
+++ BudgetBuddy.Test/TransactionTest.cs
@@ -0,0 +1,29 @@
+using BudgetBuddy.Controllers;
+using BudgetBuddy.Models;
+
+namespace BudgetBuddy.Test;
+
+[TestClass]
+public class TransactionTest
+{
+    [TestMethod]
+    public void AdjustingAccountBalance_ChangesSettledBalance()
+    {
+        // TODO: Write the test. Unsure how I want to test this
+        Assert.Fail("Test not implemented");
+    }
+
+    [TestMethod]
+    public void ChangingPendingToSettled_RecalculatesAccountBalances()
+    {
+        // TODO: Write the test. Unsure how I want to test this
+        Assert.Fail("Test not implemented");
+    }
+
+    [TestMethod]
+    public void DeletingTransaction_RecalculatesAccountBalances()
+    {
+        // TODO: Write the test. Unsure how I want to test this
+        Assert.Fail("Test not implemented");
+    }
+}
