d0057a6fc67802d48a7425879229d0fed74f0d60
TomWaa <superxgenesis@gmail.com>
2024-08-15 19:20:33 +0200

    [core] Add additional accessor methods

--- BudgetBuddy/Repositories/ICategoryRepository.cs
+++ BudgetBuddy/Repositories/ICategoryRepository.cs
@@ -4,4 +4,7 @@ namespace BudgetBuddy.Repositories;
 
 public interface ICategoryRepository : IRepository<Category>
 {
+    decimal GetAvailableAmount(int id);
+    decimal GetActivityAmount(int id);
+    decimal GetBudgetetAmount(int id);
 }
