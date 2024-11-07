6f12e885e031ab55869388bb3457cc067a2c6ce9
WaalerThomas <thomas.waaler@visma.com>
2024-11-07 13:51:38 +0100

    DB migration: Added Category table to database

---
+++ BudgetBuddy/BudgetBuddy.Common/Migrations/20241107125103_added_categories.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBuddy.Common.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ClientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    MonthlyAmount = table.Column<decimal>(type: "TEXT", nullable: true),
+                    GoalAmount = table.Column<decimal>(type: "TEXT", nullable: true),
+                    IsGroup = table.Column<bool>(type: "INTEGER", nullable: false),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
