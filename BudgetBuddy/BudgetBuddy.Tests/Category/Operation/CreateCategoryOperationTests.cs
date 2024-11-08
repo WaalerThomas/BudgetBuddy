2f14ab034fa98296bf3fc6750003abcffe6b4e18
WaalerThomas <thomas.waaler@visma.com>
2024-11-08 22:45:23 +0100

    Tests: Added for validation CreateCategory operation

---
+++ BudgetBuddy/BudgetBuddy.Tests/Category/Operation/CreateCategoryOperationTests.cs
@@ -0,0 +1,46 @@
+﻿using BudgetBuddy.Category.Operations;
+using BudgetBuddy.Category.Repositories;
+using BudgetBuddy.Category.Service;
+using BudgetBuddy.Contracts.Model.Category;
+using BudgetBuddy.Tests.Common;
+using NSubstitute;
+
+namespace BudgetBuddy.Tests.Category.Operation;
+
+[TestFixture]
+public class CreateCategoryOperationTests
+{
+    private CreateCategoryOperation _operation;
+
+    private ICategoryValidator _categoryValidator;
+    private ICategoryRepository _categoryRepository;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _categoryValidator = Substitute.For<ICategoryValidator>();
+        _categoryRepository = Substitute.For<ICategoryRepository>();
+        
+        _operation = new CreateCategoryOperation(_categoryValidator, _categoryRepository);
+    }
+
+    [Test]
+    public void CreateCategory_ShouldValidateModel()
+    {
+        // act
+        _operation.Operate(TestHelper.CreateCategory());
+        
+        // assert
+        _categoryValidator.Received().ValidateGroupAssignment(Arg.Any<CategoryModel>());
+    }
+    
+    [Test]
+    public void CreateCategory_ShouldCallCreateOnRepository()
+    {
+        // act
+        _operation.Operate(TestHelper.CreateCategory());
+        
+        // assert
+        _categoryRepository.Received().Create(Arg.Any<CategoryModel>());
+    }
+}
--- BudgetBuddy/BudgetBuddy.Tests/Category/Service/CategoryServiceTests.cs
+++ BudgetBuddy/BudgetBuddy.Tests/Category/Service/CategoryServiceTests.cs
@@ -1,6 +1,21 @@
-﻿namespace BudgetBuddy.Tests.Category.Service;
+﻿using BudgetBuddy.Category.Service;
+using BudgetBuddy.Core.Operation;
+using NSubstitute;
+
+namespace BudgetBuddy.Tests.Category.Service;
 
 [TestFixture]
 public class CategoryServiceTests
 {
+    private CategoryService _categoryService;
+    
+    private IOperationFactory _operationFactory;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _operationFactory = Substitute.For<IOperationFactory>();
+        
+        _categoryService = new CategoryService(_operationFactory);
+    }
 }
