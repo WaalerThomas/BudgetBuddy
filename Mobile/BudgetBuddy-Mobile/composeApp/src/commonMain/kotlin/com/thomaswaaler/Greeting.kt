978af7e73c093f5a3a6611955da643857936f215
WaalerThomas <superxgenesis@gmail.com>
2025-02-11 19:06:01 +0100

    New barebones

---
+++ Mobile/BudgetBuddy-Mobile/composeApp/src/commonMain/kotlin/com/thomaswaaler/Greeting.kt
@@ -0,0 +1,9 @@
+package com.thomaswaaler
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
--- Mobile/BudgetBuddy-Mobile/shared/src/commonMain/kotlin/com/thomaswaaler/budgetbuddy/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.thomaswaaler.budgetbuddy
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
